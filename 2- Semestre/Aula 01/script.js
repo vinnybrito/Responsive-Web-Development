4d3c3e8912fa52ca85fab5c371af3a97ff69e3a3
vinnybrito <viny.rbrito15@hotmail.com>
2023-02-09 16:51:58 -0300

    segundo semestre

---
+++ 2- Semestre/Aula 01/script.js
@@ -0,0 +1,13 @@
+function Logar() {
+    //Declarando as variáveis
+    let usuario = document.getElementById("usuarioid");
+    let senha = document.getElementById("senhaid");
+
+    if (usuario.value !== "" | senha.value !== "") {
+        alert("Seja bem vindo" + usuario.value);
+    }
+
+    else {
+        alert("Preencha todos os campos!")
+    }
+}
